86b5efb0f32a44390b5286a190806e1a2cd9088c
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-02-18 12:39:23 -0500

    answer to question number 1 on exam

---
+++ test2.js
@@ -0,0 +1,13 @@
+
+//Eric Heinzman
+//Triloop
+//will loop through and print 10 # and decrease by one each time
+//answering question number 1 of exam 
+
+
+var number = "#";
+//creating a variable 
+for(var number="#"; number<"###########"; number+="#")
+//for loop will run through till # is greater than 10 #'s
+print(number);
+//printing number
